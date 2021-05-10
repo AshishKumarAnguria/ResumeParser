9e2aed5e7ab73d3703a8937771353e8ba65a12d0
AshishKumarAnguria <34279453+AshishKumarAnguria@users.noreply.github.com>
2021-05-10 20:35:59 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -22,6 +22,86 @@ stanford_classifier  =  'stanford-ner-2015-04-20/classifiers/english.all.3class.
 
 #Build NER tagger object
 st = StanfordNERTagger(stanford_classifier)
+def get_total_experience(txt):
+    lines = txt.split('\n')
+    t_p=['till now','present','Till Date','to till']
+    x=datetime.now()
+    date_check=[]
+    T_exp=relativedelta.relativedelta(x,x)
+    #print("init:",T_exp)
+    sy=2021
+    ey=0
+    prev=True
+    date=[]
+    for l in lines:
+        l=l.strip()
+        l=re.sub(r'[^A-Za-z0-9 /.%]+', '', l)
+        
+        no_date=["birth",'dob','university',' board ','%','percentage','percent','cpi ','gpa ','cg ','grade','Institute',"date of birth"]
+        #print(l)
+        if re.compile('|'.join(no_date),re.IGNORECASE).search(l):
+            prev=False
+            continue
+        
+        if len(re.findall(r'\d\d\d\d\d',l))>0:
+            continue
+        if (search_dates(l)) is not None and prev==False:
+            prev=True
+            continue
+
+        q=re.findall(r'\d\d\d\d',str(l))
+        #print(len(list(q)),"->",q)    
+         
+        if len(q)>0:   
+            d=search_dates(l)
+            if d is None:
+                date=[]
+            if d is not None :
+                if re.compile('|'.join(t_p),re.IGNORECASE).search(l):
+                    d.append(('May 2021',datetime.now()))##change to current month year
+                for e in d:
+                    e=list(e)
+                    if 'in' in e[0]:
+                        continue
+                    yr=re.findall(r'\d\d\d\d',e[0])
+
+                    if len(yr)>0 and int(yr[0])<2022 and int(yr[0])>1990:
+                        
+                        y=int(yr[0])
+
+                        try:
+                            date.append(e[1])
+                            
+                            if len(date)==2:
+                                if (date[0].year,date[1].year) in date_check:
+                                    date=[]
+                                    continue
+                                
+                                #print("f3:",date_check)
+                                date_check.append((date[0].year,date[1].year))
+            
+                        except:
+                            pass
+                        sy=min(y,sy)
+                        ey=max(ey,y)
+                        #print(y)
+                try:
+                    
+                    t_experience = relativedelta.relativedelta(date[1], date[0])
+                    if len(date)>1:
+                        date=[]
+                    T_exp+=t_experience
+        
+                except:
+                    pass
+                
+                
+                #print(t)
+        
+    #print("total experience:",ey,"-",sy,"->",ey-sy)
+    #print("total experience-T:",T_exp.years," ",T_exp.months)
+    return str(T_exp.years)+"/"+str(T_exp.months)
+
 def find_name(txt):
     lines = txt.split('\n')
     c=0
@@ -62,3 +142,4 @@ for filename in sys.argv[1:]:
         print("NAME:",name)
         print("EMAIL:",emails)
         print("Contact:",corr_contact)
+        print("Total experience(year/month):",total_experience)
